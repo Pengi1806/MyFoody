807f414ebc3489c0897cb1b6e5348c1da26558f0
Nhật Hào <17110289@student.hcmute.edu.vn>
2022-04-18 12:26:08 +0700

    Giao diện màn hình chính
    Giao diện màn hình chính

--- app/src/main/java/hcmute/edu/vn/myfoody/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/myfoody/MainActivity.java
@@ -5,24 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtForgotPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
 
         txtForgotPassword = (TextView) findViewById(R.id.forgotPassword);
+        btnLogin = (Button) findViewById(R.id.buttonLogin);
 
         txtForgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,ForgotPassword1.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
                 startActivity(intent);
             }
         });

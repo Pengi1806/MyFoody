ce801d8ba3b826d47608f2e4e1056dddd9077b76
Nhật Hào <17110289@student.hcmute.edu.vn>
2022-04-24 22:54:44 +0700

    Tạo Database + Insert 1 vài dữ liệu + Đổ được dữ liệu vào Spinner
    Tạo Database + Insert 1 vài dữ liệu + Đổ được dữ liệu vào Spinner

--- app/src/main/java/hcmute/edu/vn/myfoody/ForgotPassword1.java
+++ app/src/main/java/hcmute/edu/vn/myfoody/ForgotPassword1.java
@@ -3,16 +3,24 @@ package hcmute.edu.vn.myfoody;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ForgotPassword1 extends AppCompatActivity {
 
     ImageButton imgBackForgotPassword1;
     Button btnChangePassword;
+    Spinner spinnerSecurityQuestions;
+    Database database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,21 @@ public class ForgotPassword1 extends AppCompatActivity {
 
         imgBackForgotPassword1 = (ImageButton) findViewById(R.id.imgBackForgotPassword);
         btnChangePassword = (Button) findViewById(R.id.buttonChangePassword);
+        spinnerSecurityQuestions = (Spinner) findViewById(R.id.spinnerSecurityQuestion);
+
+        ArrayList<String> arraySecurityQuestions = new ArrayList<String>();
+        arraySecurityQuestions.add("[Choose your security question]");
+        database = new Database(ForgotPassword1.this, "foody.sqlite", null, 1);
+        //get data
+        Cursor dataSecurityQuestions = database.GetData("SELECT * FROM SecurityQuestions");
+        while (dataSecurityQuestions.moveToNext()){
+            String QuestionContent = dataSecurityQuestions.getString(1);
+            arraySecurityQuestions.add(QuestionContent);
+        }
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, arraySecurityQuestions);
+        arrayAdapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        spinnerSecurityQuestions.setAdapter(arrayAdapter);
 
         imgBackForgotPassword1.setOnClickListener(new View.OnClickListener() {
             @Override

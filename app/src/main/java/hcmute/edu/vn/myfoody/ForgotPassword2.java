ced01b523c78fc86420da5fc78e849a6bfeea4a5
Nhật Hào <17110289@student.hcmute.edu.vn>
2022-04-25 14:16:38 +0700

    Hoàn tất chức năng Login + Forgot Password
    Hoàn tất chức năng Login + Forgot Password

--- app/src/main/java/hcmute/edu/vn/myfoody/ForgotPassword2.java
+++ app/src/main/java/hcmute/edu/vn/myfoody/ForgotPassword2.java
@@ -1,14 +1,28 @@
 package hcmute.edu.vn.myfoody;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ForgotPassword2 extends AppCompatActivity {
 
     ImageView imgBackForgotPassword2;
+    EditText editTextNewPasswordForgotPassword;
+    EditText editTextConfirmPasswordForgotPassword;
+    Button btnConfirmForgotPassword;
+
+    Database database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +30,55 @@ public class ForgotPassword2 extends AppCompatActivity {
         setContentView(R.layout.activity_forgot_password2);
 
         imgBackForgotPassword2 = (ImageView) findViewById(R.id.imgBackForgotPassword2);
+        editTextNewPasswordForgotPassword = (EditText) findViewById(R.id.editNewPasswordForgotPassword);
+        editTextConfirmPasswordForgotPassword = (EditText) findViewById(R.id.editConfirmPasswordForgotPassword);
+        btnConfirmForgotPassword = (Button) findViewById(R.id.buttonConfirm);
+
+        database = new Database(ForgotPassword2.this, "foody.sqlite", null, 1);
 
         imgBackForgotPassword2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btnConfirmForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email = getIntent().getStringExtra("Email");
+                String newPassword = editTextNewPasswordForgotPassword.getText().toString();
+                String confirmPassword = editTextConfirmPasswordForgotPassword.getText().toString();
+                if (newPassword.equals("") == false && confirmPassword.equals("") == false) {
+                    if (newPassword.equals(confirmPassword)) {
+                        database.QueryData("UPDATE Users SET Password = '" + newPassword + "' WHERE Email = '" + Email + "'");
+
+                        //Hiện thông báo
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ForgotPassword2.this);
+                        builder.setMessage("Mật khẩu của bạn đã được thay đổi");
+                        builder.setTitle("Thông báo");
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent(ForgotPassword2.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                            @Override
+                            public void onCancel(DialogInterface dialogInterface) {
+                                Intent intent = new Intent(ForgotPassword2.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    } else {
+                        Toast.makeText(ForgotPassword2.this, "Cần nhập mật khẩu giống nhau!!!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(ForgotPassword2.this, "Cần nhập đủ thông tin", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
